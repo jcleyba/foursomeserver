2fac0ed5b6d45604dd6fa7c93e3bfef0ce4ade10
Juan Cruz Leyba <jleyba@itx.com>
2020-03-11 11:40:55 -0300

    Updating production port
    Updating production port
    
    Updating production port
    
    Updating production port

--- index.ts
+++ index.ts
@@ -4,9 +4,12 @@ import { ApolloServer } from 'apollo-server'
 import typeDefs from './types'
 import resolvers from './resolvers'
 
-const server = new ApolloServer({ typeDefs, resolvers })
-
-// The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`)
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true,
+})
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`)
 })
