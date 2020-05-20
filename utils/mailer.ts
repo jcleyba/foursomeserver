3538654f21f988d7bcbfdac211c1008e74595762
Juan Leyba <jleyba@itx.com>
2020-05-20 13:06:11 -0300

    Fixing domain env variable.

--- utils/mailer.ts
+++ utils/mailer.ts
@@ -18,7 +18,7 @@ export function sendEmailVerification(email: string, token: string) {
     subject: '¡Bienvenido!',
     html: `<p>Gracias por registrarse en Golf Time. Para continuar, 
       por favor confirme su dirección de correo electrónico haciendo click 
-      <a href="http://localhost:3000/verify?token=${token}">aquí</a></p><p>El equipo de Golf Time</p>`,
+      <a href="${process.env.DOMAIN}/verify?token=${token}">aquí</a></p><p>El equipo de Golf Time</p>`,
   }
   // Enviamos el email
   transporter().sendMail(mailOptions, function (error: any, info: any) {
@@ -35,7 +35,7 @@ export function sendPassRecovery(email: string, token: string) {
     from: 'Golf Time <inscripciones.golftime@gmail.com>',
     to: email,
     subject: 'Recuperación de contraseña',
-    html: `<p>Para generar una nueva contraseña por favor haga click <a href="${process.env.domain}/verify?token=${token}">aquí</a></p><p>El equipo de Golf Time</p>`,
+    html: `<p>Para generar una nueva contraseña por favor haga click <a href="${process.env.DOMAIN}/verify?token=${token}">aquí</a></p><p>El equipo de Golf Time</p>`,
   }
   // Enviamos el email
   transporter().sendMail(mailOptions, function (error: any, info: any) {
